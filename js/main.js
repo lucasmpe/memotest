9c1a3a7f989ca87abd96efebc0ac3d3455042b85
lucasmp <lucasmatiasperez@gmail.com>
2021-11-26 11:02:51 -0300

    Se agragan las funcionalidades basicas de la primera version del juego

--- js/main.js
+++ js/main.js
@@ -1,63 +1,155 @@
-const cuadros = [
-    'img/aguilas-doradas.jpg',
-    'img/alianza-petrolera.jpg',
-    'img/america-de-cali.jpg',
-    'img/atletico-bucaramanga.jpg',
-    'img/atletico-huila.jpg',
-    'img/atletico-junior.jpg',
-    'img/atletico-nacional.jpg',
-    'img/deportes-quindio.jpg',
-    'img/deportes-tolima.jpg',
-    'img/deportivo-cali.jpg',
-    'img/deportivo-pasto.jpg',
-    'img/deportivo-pereira.jpg',
-    'img/envigado-fc.jpg',
-    'img/independiente-medellin.jpg',
-    'img/independiente-santa-fe.jpg',
-    'img/jaguares-de-cordoba.jpg',
-    'img/la-equidad-seguros.jpg',
-    'img/millonarios.jpg',
-    'img/once-caldas.jpg',
-    'img/patriotas-boyaca.jpg'
+const cartas = ['img/aguilas-doradas.jpg', 'img/alianza-petrolera.jpg', 'img/america-de-cali.jpg', 'img/atletico-bucaramanga.jpg',
+    'img/atletico-huila.jpg', 'img/atletico-junior.jpg', 'img/atletico-nacional.jpg', 'img/deportes-quindio.jpg',
+    'img/deportes-tolima.jpg', 'img/deportivo-cali.jpg', 'img/deportivo-pasto.jpg', 'img/deportivo-pereira.jpg',
+    'img/envigado-fc.jpg', 'img/independiente-medellin.jpg', 'img/independiente-santa-fe.jpg', 'img/jaguares-de-cordoba.jpg',
+    'img/la-equidad-seguros.jpg', 'img/millonarios.jpg', 'img/once-caldas.jpg', 'img/patriotas-boyaca.jpg'
 ];
 
-const CANTIDAD_CUADROS = cuadros.length;
-let registroAsignaciones = new Array(CANTIDAD_CUADROS);
+const CANTIDAD_CARTAS = cartas.length;
+let registroAsignaciones = new Array(CANTIDAD_CARTAS);
+let tablero = {};
+let cartaUsuario = null;
+let intentos = 0;
+let aciertos = 0;
+
+function crearTablero() {
+    const $tablero = document.querySelector('#tablero');
+    for (let i = 0; i < CANTIDAD_CARTAS * 2; i++) {
+        const $carta = document.createElement('div');
+        $carta.classList = 'carta';
+        $carta.id = `carta-${i + 1}`;
+        
+        const $imagen = document.createElement('img');
+        $imagen.classList = 'img-fluid dorso';
+        $imagen.src = 'img/dorso-carta.jpg';
+
+        $carta.appendChild($imagen);
+        $tablero.appendChild($carta);
+    }
+}
 
 function registroAsignacionesEstaLleno() {
     let suma = 0;
-    for(let i=0; i<registroAsignaciones.length; i++) {
+    for (let i = 0; i < registroAsignaciones.length; i++) {
         suma += registroAsignaciones[i];
     }
-    if(suma === CANTIDAD_CUADROS*2) {
+    if (suma === CANTIDAD_CARTAS * 2) {
         return true;
     }
     return false;
 }
 
-function asignarCuadro(imagenCarta) {
+function crearImagen() {
     if (registroAsignacionesEstaLleno()) {
-        return false;
+        return null;
     }
     const indiceAleatorio = Math.floor(Math.random() * 20);
-    if(registroAsignaciones[indiceAleatorio] < 2) {
-        imagenCarta.src = cuadros[indiceAleatorio];
+    if (registroAsignaciones[indiceAleatorio] < 2) {
+        const $imagen = document.createElement('img');
+        $imagen.classList = 'img-fluid oculto';
+        $imagen.src = cartas[indiceAleatorio];
         registroAsignaciones[indiceAleatorio]++;
-        return true;
+        return $imagen;
     } else {
-        asignarCuadro(imagenCarta)
+        return crearImagen();
     }
 }
 
 function iniciarTablero() {
-    registroAsignaciones.fill(0);    
-    for(let i=0; i<CANTIDAD_CUADROS*2; i++) {
-        const $imagenCarta = document.querySelector(`#carta-${i+1} img`); //Obtengo el elemento 'img' de cada una de las cartas
-        asignarCuadro($imagenCarta);
+    registroAsignaciones.fill(0);
+    for (let i = 0; i < CANTIDAD_CARTAS * 2; i++) {
+        const $carta = document.querySelector(`#carta-${i + 1}`);
+        const $imagen = crearImagen();
+        $carta.appendChild($imagen);
+        tablero[`carta-${i + 1}`] = $imagen.src;
     }
 }
 
-//iniciarTablero();
+function reiniciar() {
+    tablero = {};
+    cartaUsuario = null;
+    intentos = 0;
+    aciertos = 0;
+
+    iniciarJuego();
+}
+
+function girarCarta(carta) {
+    carta.childNodes.forEach(function (img) {
+        if (!img.classList.toggle('oculto')) {
+        }
+    });
+}
+
+function bloquearCartas(carta1, carta2) {
+    carta1.onclick = function() {};
+    carta2.onclick = function() {};
+}
+
+function limpiarTablero() {
+    document.querySelectorAll('.carta').forEach(function(carta) {
+        carta.remove();
+      });
+}
+
+function ganar() {
+
+    /*Hacer algo si gana*/
+    limpiarTablero();
+
+    reiniciar();
+
+}
+
+function manejarInputUsuario(e) {
+    
+    const $carta = e.target.parentNode;
+    girarCarta($carta);
+
+    if (!cartaUsuario) {
+        cartaUsuario = $carta;
+        
+    } else {
+        intentos++;
+        if (tablero[cartaUsuario.id] !== tablero[$carta.id]) {
+        
+            setTimeout(function() {
+                girarCarta($carta);
+            }, 1000);
+
+            setTimeout(function() {
+                girarCarta(cartaUsuario);
+                cartaUsuario = null;
+            }, 1000);
+
+            
+        }
+
+        if(tablero[cartaUsuario.id] === tablero[$carta.id]) {
+            aciertos++;
+            bloquearCartas(cartaUsuario, $carta);
+            cartaUsuario = null;
+            if (aciertos === 20) {
+                ganar();
+            }
+        }
+    }
+
+}
+
+
+
+function iniciarJuego() {
+    crearTablero();
+    iniciarTablero();
+    document.querySelectorAll('.carta').forEach(function ($carta) {
+        $carta.onclick = manejarInputUsuario;
+    });
+
+}
+
+iniciarJuego();
+
 
 
 
